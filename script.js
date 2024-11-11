e2ad5233d8e9a710a952ffef34c97dcbdd138b69
Matt <mcalabia@baunfire.com>
2024-11-11 15:10:19 +0800

    script update

---
+++ script.js
@@ -0,0 +1,14 @@
+function handleFormSubmission(formClassName) {
+    
+    $(formClassName).submit(function (e) { 
+        e.preventDefault(); 
+        console.log("working")
+        var form = this;  
+        var name = form.querySelector('input[name="first_name"]').value;
+        var email = form.querySelector('input[name="email_address"]').value;
+        var phone = form.querySelector('input[name="phone"]').value;
+        console.log(name +" "+ email +" "+ phone)
+    });
+ }
+ handleFormSubmission(".workshop-form-hubspot");
+ 
