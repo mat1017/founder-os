99ffe89a9127ccfb8701ae4d71a30ef57f917b36
Matt <mcalabia@baunfire.com>
2024-11-11 15:24:27 +0800

    script update

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-function handleFormSubmission(formClassName) {
+$(document).ready(function () {
+    function handleFormSubmission(formClassName) {
+        $(formClassName).submit(function (e) {
+            e.preventDefault();
+            var form = this;
+
+            // Get form field values
+            var name = form.querySelector('input[name="first_name"]').value;
+            var email = form.querySelector('input[name="email_address"]').value;
+            var phone = form.querySelector('input[name="phone"]').value;
+
+            // Log form data (for debugging)
+            console.log(name + " " + email + " " + phone);
+            var redirectUrl = `https://www.founderos.com/workshop-survey?first_name=${encodeURIComponent(name)}&email=${encodeURIComponent(email)}&phone=${encodeURIComponent(phone)}`;
+            window.location.href = redirectUrl;
+        });
+    }
     
-    $(formClassName).submit(function (e) { 
-        e.preventDefault(); 
-        console.log("working")
-        var form = this;  
-
-        console.log(form);
-        var name = form.querySelector('input[name="first_name"]').value;
-        var email = form.querySelector('input[name="email_address"]').value;
-        var phone = form.querySelector('input[name="phone"]').value;
-        console.log(name +" "+ email +" "+ phone)
-    });
- }
-
- 
- handleFormSubmission(".workshop-form-hubspot");
-
-
- console.log("data");
+    handleFormSubmission(".workshop-form-hubspot");
+});
